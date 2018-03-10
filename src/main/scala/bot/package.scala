82610fed13b0247e3de90e0df414fc7e4ed2691d
evilPaprika <ivil.paprika@gmail.com>
2018-03-10 12:10:46 +0500

    вывод из файла

--- src/main/scala/bot/package.scala
+++ src/main/scala/bot/package.scala
@@ -1,3 +1,11 @@
-object bot {
+import scala.io.Source
 
+object app {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+    val filename = "test.txt"
+    for (line <- Source.fromFile(filename).getLines) {
+      println(line)
+    }
+  }
 }
